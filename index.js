7f97ad826506278ac0339184898f1dd957736652
Hofer <hofer@Hofers-MacBook-Pro-2.local>
2023-05-10 15:31:54 +0800

    BUIDL

---
+++ index.js
@@ -0,0 +1,22 @@
+const bip39 = require("bip39");
+
+// bip39库的词典长度
+const wordlistLength = bip39.wordlists.english.length;
+
+for (let i = 0; i < wordlistLength; i++) {
+  const word = bip39.wordlists.english[i];
+  const repeatedWord = Array(12).fill(word).join(" ");
+
+  if (bip39.validateMnemonic(repeatedWord)) {
+    console.log(`${repeatedWord}`);
+  }
+}
+
+for (let i = 0; i < wordlistLength; i++) {
+  const word = bip39.wordlists.english[i];
+  const repeatedWord = Array(24).fill(word).join(" ");
+
+  if (bip39.validateMnemonic(repeatedWord)) {
+    console.log(`${repeatedWord}`);
+  }
+}
